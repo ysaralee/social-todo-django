3a3bd5b3097bd643262fffe964d337adec99360f
Yeonsoo Sara Lee <ysaralee@gmail.com>
2016-02-18 00:28:26 +0000

    Created forms for users and tasks, routed urls to each.

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'up', views.say_whatsup, name='whatsup'),
+    url(r'get_name', views.get_name, name ='getname')
 ]
 
--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'get_name', views.get_name, name ='getname')
 ]
