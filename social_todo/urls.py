5cbada851df22cb28467c887593a6a11f5d39c32
Yeonsoo Sara Lee <ysaralee@gmail.com>
2016-02-18 00:21:06 +0000

    Created users subapp, routed social_todo/urls to users index.

--- social_todo/urls.py
+++ social_todo/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^tasks/', include('tasks.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^users/', include('users.urls'))
 ]
 
